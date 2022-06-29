91e9911cbb8795843aaff64880684a9acef26800
Valdryan  Ivandito <valdryan.ivandito@berca.co.id>
2022-06-30 00:03:40 +0700

    Fourth Commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 -- IMPORT LIBRARY -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import Control.Monad.State (StateT, MonadState (get), MonadTrans (lift), execStateT)
 import Data.Time
 import Data.Char
 import System.IO
@@ -83,126 +84,145 @@ rulesBaseCheck' stockPrice roe der intrinsicValue
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- EXECUTEABLE FUNCTIONS ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+executeState :: StateT String IO ()
+executeState = do 
+               state <- get
+               lift (putStr (state))
+
 main :: IO ()
-main = login
+main = login "Value Investor"
 
-login :: IO ()
-login =  do 
-         putStrLn ("\n-- Welcome to Value Investor Calculator Application --")
-         putStrLn ("Login with your investor account")
-         putStr ("Username: ")
-         username <- getLine
-         putStr ("Password: ")
-         password <- withEcho False getLine
-         putChar '\n'
-         checkLogin username password
-
-checkLogin :: String -> String -> IO ()
-checkLogin username password 
-    | (username == "Valdryan Ivandito") && (password == "kucingterbang@618") = succeedLogin 
-    | otherwise                                                              = reLogin
-
-succeedLogin :: IO ()
-succeedLogin = do 
-               timeStamp <- getZonedTime
-               let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
-               appendFile "log_file.txt" (timeStampResult ++ ";" ++ "LOGIN" ++ ";" ++ "User succeed login " ++ "\n")
-               menu
-
-reLogin :: IO ()
-reLogin = do  
-          timeStamp <- getZonedTime
-          let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
-          appendFile "log_file.txt" (timeStampResult ++ ";" ++ "FAILED" ++ ";" ++ "User failed login " ++ "\n")
-          putStrLn ("\nInvalid Username or Password!")
-          putStrLn ("Please choose the option : (1) -> Try Again ; (Press any button) -> Quit")
-          putStr ("Option: ")
-          option <- getLine
-          case option of
-               "1" -> login 
-               _   -> return ()              
+login :: String -> IO ()
+login username = do 
+                 execStateT executeState ("\n-- Welcome to Value Investor Calculator Application --\n")
+                 execStateT executeState ("Login with your investor account\n")
+                 execStateT executeState ("Username: ")
+                 username <- getLine
+                 execStateT executeState ("Password: ")
+                 password <- withEcho False getLine
+                 execStateT executeState ("\n")
+                 checkLogin username password
 
 withEcho :: Bool -> IO a -> IO a
 withEcho echo action = do
-  old <- hGetEcho stdin
-  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
+                       old <- hGetEcho stdin
+                       bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
 
-menu :: IO ()
-menu = do 
-       putStrLn ("\nWelcome Value-Investor, Can I help you ?")
-       putStrLn ("Please choose the option : (1) Input Data ; (2) Read Data ; (3) Delete Data ; (4) Logout")
-       putStr ("Option: ")
-       option <- getLine
-       case option of
-            "1" -> inputData
-            "2" -> readData
-            "4" -> do 
-                   putStrLn ("\nSee you again... happy investing :)\n")
+checkLogin :: String -> String -> IO ()
+checkLogin username password 
+    | (username == "Valdryan Ivandito") && (password == "kucingterbang@618") = succeedLogin username
+    | otherwise                                                              = reLogin username
+
+succeedLogin :: String -> IO ()
+succeedLogin username = do 
+                        timeStamp <- getZonedTime
+                        let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
+                        appendFile "log_file.txt" (timeStampResult ++ ";" ++ "LOGIN" ++ ";" ++ username ++ " succeed login" ++ "\n")
+                        menu username
+
+reLogin :: String -> IO ()
+reLogin username = do  
                    timeStamp <- getZonedTime
                    let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
-                   appendFile "log_file.txt" (timeStampResult ++ ";" ++ "LOGOUT" ++ ";" ++ "User logged out " ++ "\n")
-                   return ()
-            _   -> menu
+                   appendFile "log_file.txt" (timeStampResult ++ ";" ++ "FAILED" ++ ";" ++ username ++ " failed login" ++ "\n")
+                   execStateT executeState ("\nInvalid Username or Password!\n")
+                   execStateT executeState ("Please choose the option : (1) -> Try Again ; (Press any button) -> Quit\n")
+                   execStateT executeState ("Option: ")
+                   option <- getLine
+                   case option of
+                        "1" -> login username
+                        _   -> return ()              
+
+menu :: String -> IO ()
+menu username = do 
+                execStateT executeState ("\nWelcome Value-Investor, Can I help you ?\n")
+                execStateT executeState ("Please choose the option : (1) -> Input Data ; (2) -> Load Data ; (3) -> Read Log ; (Press any button) -> Logout\n")
+                execStateT executeState ("Option: ")
+                option <- getLine
+                case option of
+                     "1" -> inputData username
+                     "2" -> readData username
+                     "3" -> readLog username
+                     _   -> do 
+                            timeStamp <- getZonedTime
+                            let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
+                            appendFile "log_file.txt" (timeStampResult ++ ";" ++ "LOGOUT" ++ ";" ++ username ++ " logged out" ++ "\n")
+                            execStateT executeState ("\nSee you again... happy investing :)\n")
+                            return ()
  
-inputData :: IO ()
-inputData = do 
-            timeStamp <- getZonedTime
-            putStr "CompanyName: "
-            companyName <- getLine
-            putStr "BusinessSector: "
-            businessSector <- getLine
-            putStr "StockCode: "
-            stockCode <- getLine
-            putStr "StockPrice: "
-            stockPrice <- getLine
-            putStr "StockShares: "
-            stockShares <- getLine
-            putStr "Quartal: "
-            quartal <- getLine
-            putStr "Liability: "
-            liability <- getLine
-            putStr "Equity: "
-            equity <- getLine
-            putStr "NetProfit: "
-            netProfit <- getLine
-            putStr "Inflation_10Years(%): "
-            inflation <- getLine
-            putStr "CAGR(%): "
-            cagr <- getLine
-            putStr "MOS(%): "
-            mos <- getLine
+inputData :: String -> IO ()
+inputData username = do 
+                     timeStamp <- getZonedTime
+                     execStateT executeState ("CompanyName: ")
+                     companyName <- getLine
+                     execStateT executeState ("BusinessSector: ")
+                     businessSector <- getLine
+                     execStateT executeState ("StockCode: ")
+                     stockCode <- getLine
+                     execStateT executeState ("StockPrice: ")
+                     stockPrice <- getLine
+                     execStateT executeState ("StockShares: ")
+                     stockShares <- getLine
+                     execStateT executeState ("Quartal: ")
+                     quartal <- getLine
+                     execStateT executeState ("Liability: ")
+                     liability <- getLine
+                     execStateT executeState ("Equity: ")
+                     equity <- getLine
+                     execStateT executeState ("NetProfit: ")
+                     netProfit <- getLine
+                     execStateT executeState ("Inflation_10Years(%): ")
+                     inflation <- getLine
+                     execStateT executeState ("CAGR(%): ")
+                     cagr <- getLine
+                     execStateT executeState ("MOS(%): ")
+                     mos <- getLine 
 
-            let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
-            let netProfitYear = netProfit' (read quartal) (read netProfit)
-            let roeResult = roe (netProfitYear) (read equity)
-            let derResult = der (read liability) (read equity)
-            let bvResult  = bv  (read equity) (read stockShares)
-            let pbvResult = pbv (read stockPrice) (bvResult)
-            let epsResult = eps (netProfitYear) (read stockShares)
-            let perResult = per (read stockPrice) (epsResult)
-            let intrinsicValueResult = intrinsicValue (bvResult) (epsResult) (read cagr) (read inflation)
-            let mosPriceResult = mosPrice (bvResult) (epsResult) (read cagr) (read inflation) (read mos)
-            let conclusion = rulesBase (fmap toUpper businessSector) (read stockPrice) (roeResult) (derResult) (intrinsicValueResult) 
+                     let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
+                     let netProfitYear = netProfit' (read quartal) (read netProfit)
+                     let roeResult = roe (netProfitYear) (read equity)
+                     let derResult = der (read liability) (read equity)
+                     let bvResult  = bv  (read equity) (read stockShares)
+                     let pbvResult = pbv (read stockPrice) (bvResult)
+                     let epsResult = eps (netProfitYear) (read stockShares)
+                     let perResult = per (read stockPrice) (epsResult)
+                     let intrinsicValueResult = intrinsicValue (bvResult) (epsResult) (read cagr) (read inflation)
+                     let mosPriceResult = mosPrice (bvResult) (epsResult) (read cagr) (read inflation) (read mos)
+                     let conclusion = rulesBase (fmap toUpper businessSector) (read stockPrice) (roeResult) (derResult) (intrinsicValueResult) 
 
-            putStrLn ("\n" ++ (fmap toUpper companyName) ++ " (" ++ (fmap toUpper stockCode) ++ ")" ++ " Result: ")
-            putStrLn ("BusinessSector: " ++ (fmap toUpper businessSector))
-            putStrLn ("ROE(%): " ++ show (round' roeResult))
-            putStrLn ("DER(%): " ++ show (round' derResult))
-            putStrLn ("BV: " ++ show (round bvResult))
-            putStrLn ("PBV: " ++ show (round' pbvResult))
-            putStrLn ("EPS: " ++ show (round' epsResult))
-            putStrLn ("PER: " ++ show (round' perResult))
-            putStrLn ("Intrinsic Value: " ++ show (round intrinsicValueResult))
-            putStrLn ("MOSPrice: " ++ show (round mosPriceResult))
-            putStrLn (conclusion)
-            appendFile "database_file.txt" (timeStampResult ++ ";" ++ (fmap toUpper companyName) ++ ";" ++ (fmap toUpper stockCode) ++ ";" ++ (fmap toUpper businessSector) ++ ";" ++ show (round' roeResult) ++ ";" ++ show (round' derResult) ++ ";" ++ show (round bvResult) ++ ";" ++ show (round' pbvResult) ++ ";" ++ show (round' epsResult) ++ ";" ++ show (round' perResult) ++ ";" ++ show (round intrinsicValueResult) ++ ";" ++ show (round mosPriceResult) ++ ";" ++ conclusion ++ "\n")
-            appendFile "read_file.txt" (timeStampResult ++ "\n" ++ (fmap toUpper companyName) ++ " (" ++ (fmap toUpper stockCode) ++ ")" ++ " Result: " ++ "\n" ++ "BusinessSector: " ++ (fmap toUpper businessSector) ++ "\n" ++ "ROE(%): " ++ show (round' roeResult) ++ "\n" ++ "DER(%): " ++ show (round' derResult) ++ "\n" ++ "BV: " ++ show (round bvResult) ++ "\n" ++ "PBV: " ++ show (round' pbvResult) ++ "\n" ++ "EPS: " ++ show (round' epsResult) ++ "\n" ++ "PER: " ++ show (round' perResult) ++ "\n" ++ "Intrinsic Value: " ++ show (round intrinsicValueResult) ++ "\n" ++ "MOSPrice: " ++ show (round mosPriceResult) ++ "\n" ++ conclusion ++ "\n")
-            appendFile "log_file.txt" (timeStampResult ++ ";" ++ "CREATE" ++ ";" ++ "Create data " ++ (fmap toUpper stockCode) ++ "\n")
-            menu
+                     execStateT executeState ("\n" ++ (fmap toUpper companyName) ++ " (" ++ (fmap toUpper stockCode) ++ ")" ++ " Result: " ++ "\n")
+                     execStateT executeState ("BusinessSector: " ++ (fmap toUpper businessSector) ++ "\n")
+                     execStateT executeState ("ROE(%): " ++ show (round' roeResult) ++ "\n")
+                     execStateT executeState ("DER(%): " ++ show (round' derResult) ++ "\n")
+                     execStateT executeState ("BV: " ++ show (round bvResult) ++ "\n")
+                     execStateT executeState ("PBV: " ++ show (round' pbvResult) ++ "\n")
+                     execStateT executeState ("EPS: " ++ show (round' epsResult) ++ "\n")
+                     execStateT executeState ("PER: " ++ show (round' perResult) ++ "\n")
+                     execStateT executeState ("Intrinsic Value: " ++ show (round intrinsicValueResult) ++ "\n")
+                     execStateT executeState ("MOSPrice: " ++ show (round mosPriceResult) ++ "\n")
+                     execStateT executeState (conclusion ++ "\n")
+                     
+                     appendFile "read_file.txt" (timeStampResult ++ "\n" ++ (fmap toUpper companyName) ++ " (" ++ (fmap toUpper stockCode) ++ ")" ++ " Result: " ++ "\n" ++ "BusinessSector: " ++ (fmap toUpper businessSector) ++ "\n" ++ "ROE(%): " ++ show (round' roeResult) ++ "\n" ++ "DER(%): " ++ show (round' derResult) ++ "\n" ++ "BV: " ++ show (round bvResult) ++ "\n" ++ "PBV: " ++ show (round' pbvResult) ++ "\n" ++ "EPS: " ++ show (round' epsResult) ++ "\n" ++ "PER: " ++ show (round' perResult) ++ "\n" ++ "Intrinsic Value: " ++ show (round intrinsicValueResult) ++ "\n" ++ "MOSPrice: " ++ show (round mosPriceResult) ++ "\n" ++ conclusion ++ "\n" ++ "\n")
+                     appendFile "database_file.txt" (timeStampResult ++ ";" ++ (fmap toUpper companyName) ++ ";" ++ (fmap toUpper stockCode) ++ ";" ++ (fmap toUpper businessSector) ++ ";" ++ show (round' roeResult) ++ ";" ++ show (round' derResult) ++ ";" ++ show (round bvResult) ++ ";" ++ show (round' pbvResult) ++ ";" ++ show (round' epsResult) ++ ";" ++ show (round' perResult) ++ ";" ++ show (round intrinsicValueResult) ++ ";" ++ show (round mosPriceResult) ++ ";" ++ conclusion ++ "\n")
+                     appendFile "log_file.txt" (timeStampResult ++ ";" ++ "CREATE" ++ ";" ++ username ++ " created " ++ (fmap toUpper stockCode) ++ " data" ++ "\n")
 
-readData :: IO ()
-readData = do 
-           database <- readFile "./read_file.txt"
-           print database
-           menu
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                     menu username
+
+readData :: String -> IO ()
+readData username = do 
+                    timeStamp <- getZonedTime
+                    let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
+                    appendFile "log_file.txt" (timeStampResult ++ ";" ++ "READ" ++ ";" ++ username ++ " load all data " ++ "\n")
+                    allData <- readFile "./read_file.txt"
+                    execStateT executeState (allData ++ "\n")
+                    menu username
+
+readLog :: String -> IO ()
+readLog username = do 
+                    timeStamp <- getZonedTime
+                    let timeStampResult = formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S" timeStamp
+                    appendFile "log_file.txt" (timeStampResult ++ ";" ++ "READ" ++ ";" ++ username ++ " read all log " ++ "\n")
+                    allData <- readFile "./log_file.txt"
+                    execStateT executeState (allData ++ "\n")
+                    menu username
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
